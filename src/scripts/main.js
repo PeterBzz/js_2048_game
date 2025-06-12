6f7107fa89899ef1718205feef5e2a0b54a6f159
Petro Kustov <petr.kustov@skyrora.com>
2025-06-12 15:47:00 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,10 @@
 // Uncomment the next lines to use your game instance in the browser
 const Game = require('../modules/Game.class');
 const game = new Game([
-  [0, 2, 32, 16],
-  [64, 32, 16, 8],
-  [32, 16, 8, 4],
-  [16, 8, 4, 2],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
 ]);
 
 game.getScore();
